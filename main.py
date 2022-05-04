16ce00d571dda21bdfe38e20a61509563f74cca5
Mateus Souza <mateussouza@luizalabs.com>
2022-05-04 16:48:42 -0300

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from fastapi import FastAPI
+import uvicorn
+import settings
+
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+def main():
+
+    uvicorn.run(
+        "main:app",
+        host=settings.SERVICE_HOST,
+        port=settings.SERVICE_PORT,
+        workers=settings.WORKERS_AMOUNT
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+
+SERVICE_PORT = os.getenv('SERVICE_PORT', 8000)
+SERVICE_HOST = os.getenv('SERVICE_HOST', '0.0.0.0')
+WORKERS_AMOUNT = os.getenv('WORKERS_AMOUNT', 4)
